16fc4b1481212867b34a9de240506671fa154efd
MarceloLuz1 <152748484+MarceloLuz1@users.noreply.github.com>
2023-12-13 23:31:07 -0300

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -35,6 +35,19 @@ class DoublyLinkedList:
         else:
             raise IndexError("Queue is empty")
 
+    def bubble_sort(self):
+        if self.head is None:
+            return
+        swapped = True
+        while swapped:
+            swapped = False
+            current = self.head
+            while current.next is not None:
+                if current.data > current.next.data:
+                    current.data, current.next.data = current.next.data, current.data
+                    swapped = True
+                current = current.next
+
     def search_binary(self, target):
         current = self.head
         index = 0
@@ -44,6 +57,13 @@ class DoublyLinkedList:
             current = current.next
             index += 1
         return -1
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        return
 
     def __len__(self):
         count = 0
@@ -58,13 +78,6 @@ class DoublyLinkedList:
         while current is not None:
             yield current.data
             current = current.next
-            
-    def display(self):
-        current = self.head
-        while current:
-            print(current.data, end=" ")
-            current = current.next
-        return
 
     def reverse_iterator(self):
         current = self.tail.prev
@@ -93,60 +106,54 @@ velocidade_jogo = 15
 
 
 def gerar_comida():
-    comida_x = round(random.randrange(0, largura - tamanho_quadrado) / float(tamanho_quadrado)) * float(
-        tamanho_quadrado)
 
-    comida_y = round(random.randrange(0, altura - tamanho_quadrado) / float(tamanho_quadrado)) * float(tamanho_quadrado)
+    comida_x = random.randint(0, largura - tamanho_quadrado) // tamanho_quadrado * tamanho_quadrado
+    comida_y = random.randint(0, altura - tamanho_quadrado) // tamanho_quadrado * tamanho_quadrado
 
     return comida_x, comida_y
 
 
 def desenhar_comida(tamanho, comida_x, comida_y):
+
     pygame.draw.rect(tela, (255, 0, 0), [comida_x, comida_y, tamanho, tamanho])
 
 
 def desenhar_cobra(tamanho, pixels):
+
     for pixel in pixels:
         pygame.draw.rect(tela, (0, 255, 0), [pixel[0], pixel[1], tamanho, tamanho])
 
 
 def desenhar_pontuacao(pontuacao):
+
     fonte = pygame.font.SysFont("Helvetica", 35)
-
     texto = fonte.render(f"Pontos: {pontuacao}", True, (255, 255, 255))
-
     tela.blit(texto, [1, 1])
 
 
 def selecionar_velocidade(tecla):
+
     if tecla == pygame.K_DOWN:
-
         velocidade_x = 0
-
         velocidade_y = tamanho_quadrado
 
     elif tecla == pygame.K_UP:
-
         velocidade_x = 0
-
         velocidade_y = -tamanho_quadrado
 
     elif tecla == pygame.K_RIGHT:
-
         velocidade_x = tamanho_quadrado
-
         velocidade_y = 0
 
     elif tecla == pygame.K_LEFT:
-
         velocidade_x = -tamanho_quadrado
-
         velocidade_y = 0
 
     return velocidade_x, velocidade_y
 
 
 def rodar_jogo():
+
     fim_jogo = False
 
     x = largura / 2
@@ -164,17 +171,11 @@ def rodar_jogo():
     comida_x, comida_y = gerar_comida()
 
     while not fim_jogo:
-
         tela.fill((0, 0, 0))
-
         for evento in pygame.event.get():
-
             if evento.type == pygame.QUIT:
-
                 fim_jogo = True
-
             elif evento.type == pygame.KEYDOWN:
-
                 velocidade_x, velocidade_y = selecionar_velocidade(evento.key)
 
         # desenhar_comida
@@ -217,7 +218,6 @@ def rodar_jogo():
 
         if x == comida_x and y == comida_y:
             tamanho_cobra += 1
-
             comida_x, comida_y = gerar_comida()
 
         relogio.tick(velocidade_jogo)
